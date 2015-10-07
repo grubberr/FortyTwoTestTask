4634adc0da8bae7ff9d6807b752cd8aae117ec32
Sergey Chvalyuk <grubberr@gmail.com>
2015-10-07 16:40:32 +0300

    ticket:1 model improved 200 -> 256 :)

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 
 class Contact(models.Model):
-    name = models.CharField(max_length=200)
-    surname = models.CharField(max_length=200)
+    name = models.CharField(max_length=256)
+    surname = models.CharField(max_length=256)
     date_of_birth = models.DateField()
     bio = models.TextField()
     other_contacts = models.TextField()
