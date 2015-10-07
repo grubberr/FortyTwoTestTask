981222e113a0b5e8af72f2c0f0d04a7a2a67c600
Sergey Chvalyuk <grubberr@gmail.com>
2015-10-07 13:04:40 +0300

    ticket:1 fixing flake8 warns

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -3,6 +3,7 @@ from apps.hello.models import Contact
 
 # Create your views here.
 
+
 def index(request):
     contact = Contact.objects.get()
     return render(request, 'hello/index.html', {'contact': contact})
