8c838730b23df268ea58bcfa9cb7b0f9f29c4d38
Sergey Chvalyuk <grubberr@gmail.com>
2015-10-07 14:58:44 +0300

    ticket:1 south migrations added

---
+++ apps/hello/migrations/0001_initial.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Contact'
+        db.create_table(u'hello_contact', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('surname', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('date_of_birth', self.gf('django.db.models.fields.DateField')()),
+            ('bio', self.gf('django.db.models.fields.TextField')()),
+            ('other_contacts', self.gf('django.db.models.fields.TextField')()),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
+            ('jabber', self.gf('django.db.models.fields.EmailField')(max_length=75)),
+            ('skype', self.gf('django.db.models.fields.CharField')(max_length=200)),
+        ))
+        db.send_create_signal(u'hello', ['Contact'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Contact'
+        db.delete_table(u'hello_contact')
+
+
+    models = {
+        u'hello.contact': {
+            'Meta': {'object_name': 'Contact'},
+            'bio': ('django.db.models.fields.TextField', [], {}),
+            'date_of_birth': ('django.db.models.fields.DateField', [], {}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'jabber': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'other_contacts': ('django.db.models.fields.TextField', [], {}),
+            'skype': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'surname': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        }
+    }
+
+    complete_apps = ['hello']
