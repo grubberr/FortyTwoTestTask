7a909adf35c0f28ed20ef839d3647cc75b751587
Sergey Chvalyuk <grubberr@gmail.com>
2015-10-15 13:22:51 +0300

    ticket:1 added data migration for loading initial_data.json

---
+++ apps/hello/migrations/0003_load_initial_data.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        "loaddata initial_data.json"
+        from django.core.management import call_command
+        call_command('loaddata', 'initial_data.json')
+
+    def backwards(self, orm):
+        "Write your backwards methods here."
+
+    models = {
+        u'hello.contact': {
+            'Meta': {'object_name': 'Contact'},
+            'bio': ('django.db.models.fields.TextField', [], {}),
+            'date_of_birth': ('django.db.models.fields.DateField', [], {}),
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '75'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'jabber': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'other_contacts': ('django.db.models.fields.TextField', [], {}),
+            'skype': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'surname': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        }
+    }
+
+    complete_apps = ['hello']
+    symmetrical = True
